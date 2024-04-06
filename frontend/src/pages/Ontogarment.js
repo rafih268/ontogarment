c594bb7f64ded78b7143b12e54516ae8989ee42f
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-06 12:16:43 +0100

    feat(navbar): created initial structure for products on home page

--- frontend/src/pages/Ontogarment.js
+++ frontend/src/pages/Ontogarment.js
@@ -1,7 +1,22 @@
+import { Row, Col } from 'react-bootstrap';
+
 function HomePage() {
   return (
-    <h1>Welcome to the Ontogarment store</h1>
-  )
+    <>
+      <h1>Welcome to the Ontogarment store</h1>
+      <Row>
+        <Col align='center'>
+          <h1>Product</h1>
+        </Col>
+        <Col align='center'>
+          <h1>Product</h1>
+        </Col>
+        <Col align='center'>
+          <h1>Product</h1>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default HomePage;
