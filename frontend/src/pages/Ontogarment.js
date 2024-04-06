1e3788b4017c8d0f9f9638d8505316bae6922821
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-06 14:54:08 +0100

    feat(navbar): added ProductCard component to home page

--- frontend/src/pages/Ontogarment.js
+++ frontend/src/pages/Ontogarment.js
@@ -1,19 +1,17 @@
 import { Row, Col } from 'react-bootstrap';
+import { productsArray } from '../productsStore';
+import ProductCard from '../components/ProductCard'
 
 function HomePage() {
   return (
     <>
-      <h1>Welcome to the Ontogarment store</h1>
-      <Row>
-        <Col align='center'>
-          <h1>Product</h1>
-        </Col>
-        <Col align='center'>
-          <h1>Product</h1>
-        </Col>
-        <Col align='center'>
-          <h1>Product</h1>
-        </Col>
+      <h1 align="center" className="welcome">Welcome to the Ontogarment store</h1>
+      <Row xs={1} md={3} className="rowStyle">
+        {productsArray.map((product, index) => (
+          <Col align="center" key={index}>
+            <ProductCard product={product} />
+          </Col>
+        ))}
       </Row>
     </>
   );
