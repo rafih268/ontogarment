60659e8e7fa63288b64c3c02a6992136e4753ab1
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-06 11:45:09 +0100

    feat(navbar): added routing for the pages created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,22 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/Navbar';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from './pages/Ontogarment';
+import Cancel from './pages/Cancellation';
+import Success from './pages/Success';
 
 function App() {
   return (
     <Container>
       <NavbarComponent></NavbarComponent>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }
