f7e117a73e6db364a345cc2ecb23ea8b7b0fc250
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-06 12:13:52 +0100

    feat(navbar): added array to store list of products to be used

---
+++ frontend/src/productsStore.js
@@ -0,0 +1,19 @@
+const productsArray = [
+  {
+    id: '1',
+    title: 'White Slim T-shirt',
+    price: 18.99,
+  },
+  {
+    id: '2',
+    title: 'The North Face T-shirt',
+    price: 24,
+  },
+  {
+    id: '3',
+    title: 'Crew Neck T-Shirt',
+    price: 10.95,
+  },
+];
+
+export { productsArray };
