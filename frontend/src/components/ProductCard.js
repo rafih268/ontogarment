e884ef32f7d09ec29fd2496b13f8b9a8319206d2
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-06 18:31:15 +0100

    feat(navbar): added cart context to ProductCard component

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,17 +1,24 @@
 import { Card, Button, Form, Row, Col } from 'react-bootstrap';
+import { CartContext } from '../CartContext';
+import { useContext } from 'react';
 
 function ProductCard(props) {
   const product = props.product;
+  const cart = useContext(CartContext);
+  const productQuantity = cart.getProductQuantity(product.id);
+  console.log(cart.items);
 
   return (
     <Card>
       <Card.Body>
         <Card.Title>{product.title}</Card.Title>
         <Card.Text>£{product.price}</Card.Text>
-        <Button variant="primary">Add to Cart</Button>
+        <Button variant='primary' onClick={() => cart.addOneToCart(product.id)}>
+          Add to Cart
+        </Button>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
 export default ProductCard;
