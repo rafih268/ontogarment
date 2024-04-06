01ce117acb7b3436901259985b2d595185f6cb29
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-06 18:09:36 +0100

    feat(navbar): added cart context provider to the routing system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,26 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/Navbar';
 import { Container } from 'react-bootstrap';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Ontogarment';
 import Cancel from './pages/Cancellation';
 import Success from './pages/Success';
+import CartProvider from './CartContext';
 
 function App() {
   return (
-    <Container>
-      <NavbarComponent></NavbarComponent>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="success" element={<Success />} />
-          <Route path="cancel" element={<Cancel />} />
-        </Routes>
-      </BrowserRouter>
-    </Container>
+    <CartProvider>
+      <Container>
+        <NavbarComponent></NavbarComponent>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path='success' element={<Success />} />
+            <Route path='cancel' element={<Cancel />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </CartProvider>
   );
 }
 
