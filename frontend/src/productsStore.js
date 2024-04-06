28384729cf376ef9677b4aaa9934c7a2edb90e09
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-06 12:23:07 +0100

    feat(navbar): implemented function to get product data from its id

--- frontend/src/productsStore.js
+++ frontend/src/productsStore.js
@@ -16,4 +16,14 @@ const productsArray = [
   },
 ];
 
-export { productsArray };
+function getProductData(id) {
+  let productData = productsArray.find(product => product.id === id);
+
+  if (productData == undefined) {
+    console.log("Product data does not exist for ID: " + id);
+  }
+
+  return productData;
+}
+
+export { productsArray, getProductData };
