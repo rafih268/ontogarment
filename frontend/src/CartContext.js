6a7c7bfbba25f1101f61fa375ab0955857ed5484
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-06 17:23:22 +0100

    feat(navbar): implemented logic for addOneToCart

--- frontend/src/CartContext.js
+++ frontend/src/CartContext.js
@@ -8,21 +8,45 @@ export const CartContext = createContext({
   removeOneFromCart: () => {},
   deleteFromCart: () => {},
   getTotalCost: () => {},
-})
+});
 
-export function CartProvider({children}) {
+export function CartProvider({ children }) {
   const [cartProducts, setCartProducts] = useState([]);
 
   function getProductQuantity(id) {
-    const quantity = cartProducts.find(product => product.id === id)?.quantity;
+    const quantity = cartProducts.find(
+      (product) => product.id === id
+    )?.quantity;
 
     if (quantity === undefined) {
       return 0;
     }
 
-    return quantity
+    return quantity;
   }
- 
+
+  function addOneToCart(id) {
+    const quantity = getProductQuantity(id);
+
+    if (quantity === 0) {
+      setCartProducts([
+        ...cartProducts,
+        {
+          id: id,
+          quantity: 1,
+        },
+      ]);
+    } else {
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity + 1 }
+            : product
+        )
+      );
+    }
+  }
+
   const contextValue = {
     items: cartProducts,
     getProductQuantity,
@@ -30,11 +54,9 @@ export function CartProvider({children}) {
     removeOneFromCart,
     deleteFromCart,
     getTotalCost,
-  }
+  };
 
   return (
-    <CartContext.Provider value={contextValue}>
-      {children}
-    </CartContext.Provider>
-  )
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
 }
