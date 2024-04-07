4936fb5edb9eefc5e125e357a0e6722263b9b67d
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-07 02:42:36 +0100

    feat(stripe): implemented checkout event when confirm order button is clicked

--- frontend/src/productsStore.js
+++ frontend/src/productsStore.js
@@ -1,16 +1,17 @@
+// ID for each item corresponds to the price ID in our stripe account
 const productsArray = [
   {
-    id: '1',
+    id: 'price_1P2j9NLDKlYtsvc4SNdBkslu',
     title: 'White Slim T-shirt',
     price: 18.99,
   },
   {
-    id: '2',
+    id: 'price_1P2jB5LDKlYtsvc4A26szSec',
     title: 'The North Face T-shirt',
     price: 24,
   },
   {
-    id: '3',
+    id: 'price_1P2jXiLDKlYtsvc42A2eitbw',
     title: 'Crew Neck T-Shirt',
     price: 10.95,
   },
