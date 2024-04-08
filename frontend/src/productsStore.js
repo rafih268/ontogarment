2aa25a96bab432e6843f520aed1a88ba4de7381c
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2024-04-08 18:55:08 +0100

    feat(sparql): implemented function to query the product items from the onotology

--- frontend/src/productsStore.js
+++ frontend/src/productsStore.js
@@ -1,21 +1,53 @@
-// ID for each item corresponds to the price ID in our stripe account
-const productsArray = [
-  {
-    id: 'price_1P2j9NLDKlYtsvc4SNdBkslu',
-    title: 'White Slim T-shirt',
-    price: 18.99,
-  },
-  {
-    id: 'price_1P2jB5LDKlYtsvc4A26szSec',
-    title: 'The North Face T-shirt',
-    price: 24,
-  },
-  {
-    id: 'price_1P2jXiLDKlYtsvc42A2eitbw',
-    title: 'Crew Neck T-Shirt',
-    price: 10.95,
-  },
-];
+const productsArray = queryProductData();
+
+console.log(productsArray);
+
+// SPARQL query retrieving array of items from the ontology
+function queryProductData() {
+  return new Promise((resolve, reject) => {
+    const sparqlQuery = `
+            PREFIX ontogarment: <http://www.semanticweb.org/kg/ontogarment#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            Select distinct ?id ?title ?price
+            where{
+              ?x rdfs:subClassOf <http://purl.org/opdm/garment#Garment> .
+              ?garmentInstance rdf:type ?x ;
+  	            ontogarment:id ?id ;
+  	            ontogarment:title ?title ;
+  	            ontogarment:price ?price .
+            }
+            `;
+    const endpointUrl =
+      'http://localhost:3001/blazegraph/namespace/ontogarment/sparql';
+
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/sparql-query');
+    headers.append('Accept', 'application/sparql-results+json');
+
+    fetch(endpointUrl, {
+      method: 'POST',
+      headers: headers,
+      body: sparqlQuery,
+    })
+      .then((response) => response.json()) // data returned from fetch
+      .then((data) => {
+        const resultsArray = data.results.bindings;
+
+        const valueArray = resultsArray.map((bindings) => ({
+          id: bindings.id.value,
+          title: bindings.title.value,
+          price: parseFloat(bindings.price.value),
+        }));
+
+        resolve(valueArray);
+      })
+      .catch((error) => {
+        console.error('Error querying SPARQL endpoint:', error);
+        reject(error);
+      });
+  });
+}
 
 function getProductData(id) {
   let productData = productsArray.find(product => product.id === id);
@@ -27,4 +59,5 @@ function getProductData(id) {
   return productData;
 }
 
-export { productsArray, getProductData };
+
+export { productsArray, getProductData, queryProductData };
